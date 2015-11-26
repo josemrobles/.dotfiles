1776b98f07a462b18b98cc4f321c195a1759137e
Jose Robles <joserobles@robi.attlocal.net>
2015-11-25 21:49:48 -0500

    Bash file linking

---
+++ git.bash
@@ -0,0 +1,4 @@
+# Git
+alias gs='git status'
+alias gb='git branch'
+alias ga='git add .'
---
+++ servers.bash
@@ -0,0 +1,3 @@
+# Servers
+alias chef='ssh jrobles@i.chef.realtimeprocess.net'
+alias jmr='ssh jrobles@jrobles.net -p 2222'
