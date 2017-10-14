a2e3663eea6dad506f7eb4fef0afc705b2b491b9
Jose Robles <jose.robles@1ksfl-mbp-jose.robles>
2017-10-14 09:46:21 -0400

    cleanup

--- install.sh
+++ install.sh
@@ -3,31 +3,22 @@
 # git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
 
 # Install my .vimrc 
-echo "- Checking for my .vimrc file"
-grep -q "ROBI" ~/.vimrc
-if [ $? -eq 1 ] ; then
-	echo "   - Installing my .vimrc file"
-	pwd=$(pwd)
-	mv ~/.vimrc ~/.vimrc.BAK
-	ln -s $pwd/vim/vimrc ~/.vimrc
-else
+if [ -f ~/.vimrc ]; then
 	echo "   - My .vimrc found"
+else
+	echo "   - Installing my .vimrc file"
+	ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 fi
 
-# Install Jellybeans color scheme
-echo "- Checking for vim colors dir:"
-if [ -d ~/.vim/colors ]; then
-	echo "   - Directory exists, checking for jellybeans.vim"
-	if [ -f ~/.vim/colors/jellybeans.vim ]; then
-		echo "   - File exists"
-	else
-		echo "File does not exist, downloading"
-		cd ~/.vim/colors;
-		git clone git@github.com:nanotech/jellybeans.vim.git
-	fi
-else
-	echo " ~/.vim/colors does not exist, creating it & installing jellybeans.vim"
-	mkdir -p ~/.vim/colors;
-	cd ~/.vim/colors;
-	git clone git@github.com:nanotech/jellybeans.vim.git
-fi
+# Adding some color
+#if [ -d ~/.vim/colors ]; then
+#	echo "   - Colors directory exists, backing up..."
+#	tar -czvf ~/colors-backup-DATE.tgz ~/.vim/colors
+#	rm -fr ~/.vim/colors
+#	cd ~/.vim
+#	git clone git@github.com:josemrobles/colors.git
+#else
+#	echo "   - No Colors present..."
+#	cd ~/.vim/
+#	git clone git@github.com:josemrobles/colors.git
+#fi
